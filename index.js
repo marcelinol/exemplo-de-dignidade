47571fee17d05e9b4ddd9fabde94d770915280dd
Luciano Medeiros Marcelino <lucianom@nulogy.com>
2019-09-02 13:18:15 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const query = `query getFaustaoTime {
+  currentTime {
+    minutes
+  }
+}`;
+
+fetch('http://localhost:3000/graphql', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    query
+  })
+})
+  .then(r => r.json())
+  .then(data => handleSuccess(data.data))
+
+function handleSuccess(data) {
+  const formatter = new FaustaoTimeFormatter(data.currentTime.minutes)
+  new DOMManipulator(document).writeCurrentFaustaoTime(formatter.formattedMinutes())
+}
+
+class DOMManipulator {
+  constructor(dom) {
+    this.dom = dom
+  }
+
+  writeCurrentFaustaoTime(text) {
+    const element = this.dom.getElementById("time-in-faustao-format")
+    element.innerText = text
+    return
+  }
+}
+
+class FaustaoTimeFormatter {
+  constructor(minutes) {
+    this.minutes = minutes
+  }
+
+  // TODO: Add tests
+  formattedMinutes() {
+    return `${this.minutes} minutos depois das 8:07 bicho.`
+  }
+}
